c1829aa0ee45033c04114cacbc61fecdc822d856
Elivelton <eliveltongsoficial@gmail.com>
2021-02-07 23:15:25 -0300

    iniciando com a categoriaModel

---
+++ app/entities/Categoria.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace app\entities;
+
+class Categoria extends EntityBase {
+
+    private string $descricao;
+    private Usuario $usuario;
+    private \DateTime $data;
+
+    public function setDescricao($value) {
+        $this->descricao = $value;
+    }
+
+    public function getDescricao() {
+        return $this->descricao;
+    }
+
+    public function setUsuario(Usuario $usuario) {
+        $this->usuario = $usuario;
+    }
+
+    public function getUsuario() {
+        return $this->usuario;
+    }
+
+    public function setData($value) {
+        $this->data = $value;
+    }
+
+    public function getData() {
+        return $this->data;
+    }
+
+}
